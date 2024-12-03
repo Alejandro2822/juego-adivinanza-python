6464432c6812c9299a58e662c469a14e2828bc2c
Alejandro <lacalle.alejandro@gmail.com>
2024-12-03 19:17:12 -0300

    Hicimos un juego de adivinanza en Python

---
+++ juegoadivinanza.py
@@ -0,0 +1,32 @@
+
+
+import random
+
+
+numero_secreto = random.randint(1,101)
+cant_intentos = 0
+cant_max_intentos = 10
+adivinado = False
+
+print("Bienvenido al juego de adivinar el número secreto")
+
+while not adivinado:
+    if not cant_intentos < cant_max_intentos:
+        print("¡Game Over! No has podido adivinar dentro de los 10 intentos")
+        break
+    
+    entrada = int(input("Introduce un número del 1 al 99: ")) 
+    numero = int(entrada)
+
+    if numero == numero_secreto:
+        print("¡Felicitaciones!. Has adivinado el número secreto")
+        adivinado = True
+    elif numero < numero_secreto:
+        print("El número es mayor al ingresado")
+    else:
+        print("El número es menor al ingresado")
+
+    cant_intentos += 1
+
+
+
